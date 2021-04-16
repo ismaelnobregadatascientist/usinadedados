458a3a8b64e6bbb8b5922607d29da2a2105cca0f
Ismael da Nóbrega da Silva <ismael@pb.sebrae.corp>
2021-04-15 21:59:58 -0300

    Adicionado config secure true

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const config = {
 //Enter your app config here..
   host: 'qap.sebrae.com.br',
-  secure: false,
+  secure: true,
   port: 443,
   prefix: '',
   appId: 'd183a25e-de1d-47a0-b2a0-459dfbbc20d1'
