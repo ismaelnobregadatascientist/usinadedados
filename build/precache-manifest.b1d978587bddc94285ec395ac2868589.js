60fb36aa0b1c93da1d5e158099ebc32373878060
Ismael da Nóbrega da Silva <ismael@pb.sebrae.corp>
2021-04-17 11:42:12 -0300

    Publicar no Github Pages

---
+++ build/precache-manifest.b1d978587bddc94285ec395ac2868589.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39da7fff41594af75ee44aa933cff1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce14df52e99883510dd3",
+    "url": "/static/css/main.18e90615.chunk.css"
+  },
+  {
+    "revision": "8bfdc12ad44b9978921c",
+    "url": "/static/js/2.3d013671.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.3d013671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce14df52e99883510dd3",
+    "url": "/static/js/main.ce38ed03.chunk.js"
+  },
+  {
+    "revision": "eb744c7710e8aff952bb",
+    "url": "/static/js/runtime-main.7d5e4882.js"
+  }
+]);
