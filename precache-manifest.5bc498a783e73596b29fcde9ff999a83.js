9bace278625ad7d801ea0526cc891411eb6f3b5f
Ismael da Nóbrega da Silva <ismael@pb.sebrae.corp>
2021-04-17 11:43:38 -0300

    Updates

---
+++ precache-manifest.5bc498a783e73596b29fcde9ff999a83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d4d8435a1f8ef0d39b826d72322dce",
+    "url": "/usinadedados/index.html"
+  },
+  {
+    "revision": "ce14df52e99883510dd3",
+    "url": "/usinadedados/static/css/main.18e90615.chunk.css"
+  },
+  {
+    "revision": "5c6deb5008530ebbd429",
+    "url": "/usinadedados/static/js/2.7a86fae9.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/usinadedados/static/js/2.7a86fae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce14df52e99883510dd3",
+    "url": "/usinadedados/static/js/main.ce38ed03.chunk.js"
+  },
+  {
+    "revision": "e1e7d2dcfcbed7c248cf",
+    "url": "/usinadedados/static/js/runtime-main.e1d10afc.js"
+  }
+]);
