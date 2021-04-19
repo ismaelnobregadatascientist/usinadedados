c5c71da7e127d54c6e6fcb87653c720af2c0c647
Ismael da Nóbrega da Silva <ismael@pb.sebrae.corp>
2021-04-19 13:33:55 -0300

    Atualizar dependencia react-scripts

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export const config = {
 
   host: 'qap.sebrae.com.br',
-  secure: false,
-  port: 80,
+  secure: true,
+  port: 443,
   prefix: '',
   appId: 'd183a25e-de1d-47a0-b2a0-459dfbbc20d1'
  
